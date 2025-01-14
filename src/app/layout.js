40daf4f292f2c1b105b8326ed174d23f55d59fde
Samuel Olusanya <95101113+Jester8@users.noreply.github.com>
2025-01-14 09:05:46 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,26 @@
-// Add "use client" to indicate this component uses client-side functionality like useState
+
 "use client";
 
-import React, { useState, useEffect } from 'react'; // Import React hooks
+import React, { useState, useEffect } from 'react'; 
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { DarkModeProvider } from "../app/components/context/Darkmode";
 import Topbar from "./components/Topbar";
 import Footer from "./components/Footer";
-import Loader from '../app/components/ui/Loader'; // Import the Loader component
+import Loader from '../app/components/ui/Loader'; 
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   const [loading, setLoading] = useState(true);
 
-  // Simulate loading state (you can replace this with actual loading logic)
+ 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setLoading(false); // Hide loader after 3 seconds
+      setLoading(false); 
     }, 3000);
 
-    return () => clearTimeout(timer); // Cleanup timeout on component unmount
+    return () => clearTimeout(timer); 
   }, []);
 
   return (
@@ -31,11 +31,11 @@ export default function RootLayout({ children }) {
         <DarkModeProvider>
           <div className="min-h-screen bg-gray-100 text-black dark:bg-black dark:text-white">
             {loading ? (
-              <Loader /> // Show the loader while loading is true
+              <Loader /> 
             ) : (
               <>
                 <Topbar />
-                {children} {/* Render the page content */}
+                {children} 
                 <Footer />
               </>
             )}
